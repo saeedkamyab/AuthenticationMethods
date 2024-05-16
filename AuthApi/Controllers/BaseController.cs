3b008e08dccd366d3328d339ef55ec21411aebd1
saeedkamyab <saeed.kamyab@yahoo.com>
2024-05-16 17:44:56 +0330

    می خواهم روی روش جدید مثل برای شرکت کار کتم

---
+++ AuthApi/Controllers/BaseController.cs
@@ -0,0 +1,18 @@
+﻿using AuthApi.HttpHelper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+
+        [HttpPost]
+        //public IActionResult LoadList([FromBody] CustomRequestMessage request)
+        //{
+
+        //}
+    }
+}
---
+++ AuthApi/SpecialMethod/HttpHelper/CustomRequestMessage.cs
@@ -0,0 +1,20 @@
+﻿namespace AuthApi.SpecialMethod.HttpHelper
+{
+    public class CustomRequestMessage
+    {
+        public string Data { get; set; }
+        public List<string>? Includes { get; set; }
+        public string? Expression { get; set; }
+
+
+
+        public CustomRequestMessage() { }
+
+        public CustomRequestMessage(string data)
+        {
+            Data = data;
+        }
+
+
+    }
+}
